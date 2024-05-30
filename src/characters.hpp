65e2d96083470eb6e3ed0511e0d26a88da961f53
Grubre <ogrodowczykjakub@gmail.com>
2024-05-31 00:13:37 +0200

    Added FollowPathState

--- src/characters.hpp
+++ src/characters.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "character_generator.hpp"
+#include "components/common.hpp"
 #include <entt.hpp>
 
 namespace an {
@@ -8,7 +9,9 @@ namespace an {
 struct Character {};
 
 inline entt::entity make_character(entt::registry &registry, const CharacterTraits& traits) {
-
+    auto entity = registry.create();
+    an::emplace<an::Character>(registry, entity);
+    return entity;
 }
 
 inline void turn_into_anomaly(entt::registry &registry, entt::entity character, const AnomalyTraits& traits) {
